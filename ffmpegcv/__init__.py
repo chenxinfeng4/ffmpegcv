c00da8c4e830d286fe15f55d254d095daa4841fb
chenxinfeng <cxf529125853@163.com>
2023-05-11 01:20:41 +0800

    linting code

--- ffmpegcv/__init__.py
+++ ffmpegcv/__init__.py
@@ -7,48 +7,60 @@ from subprocess import DEVNULL, check_output
 
 
 def _check():
-    if not shutil.which('ffmpeg') or not shutil.which('ffprobe'):
-        raise RuntimeError('The ffmpeg is not installed. \n\n'
-                           'Please install ffmpeg via:\n    '
-                           'conda install ffmpeg')
+    if not shutil.which("ffmpeg") or not shutil.which("ffprobe"):
+        raise RuntimeError(
+            "The ffmpeg is not installed. \n\n"
+            "Please install ffmpeg via:\n    "
+            "conda install ffmpeg"
+        )
+
 
 _check()
 
 _check_nvidia_init = None
 
+
 def _check_nvidia():
     global _check_nvidia_init
     run = lambda x: check_output(x, shell=True, stderr=DEVNULL)
     if _check_nvidia_init is None:
-        calling_output = run('ffmpeg -h encoder=hevc_nvenc')
-        if 'AVOptions' not in calling_output.decode('utf-8'):
-            raise RuntimeError('The ffmpeg is not compiled with NVENC support.\n\n'
-                               'Please re-compile ffmpeg following the instructions at:\n    '
-                               'https://docs.nvidia.com/video-technologies/video-codec-sdk/ffmpeg-with-nvidia-gpu/')
+        calling_output = run("ffmpeg -h encoder=hevc_nvenc")
+        if "AVOptions" not in calling_output.decode("utf-8"):
+            raise RuntimeError(
+                "The ffmpeg is not compiled with NVENC support.\n\n"
+                "Please re-compile ffmpeg following the instructions at:\n    "
+                "https://docs.nvidia.com/video-technologies/video-codec-sdk/ffmpeg-with-nvidia-gpu/"
+            )
 
-        calling_output = run('ffmpeg -h decoder=hevc_cuvid')
-        if 'AVOptions' not in calling_output.decode('utf-8'):
-            raise RuntimeError('The ffmpeg is not compiled with NVENC support.\n\n'
-                               'Please re-compile ffmpeg following the instructions at:\n    '
-                               'https://docs.nvidia.com/video-technologies/video-codec-sdk/ffmpeg-with-nvidia-gpu/')
+        calling_output = run("ffmpeg -h decoder=hevc_cuvid")
+        if "AVOptions" not in calling_output.decode("utf-8"):
+            raise RuntimeError(
+                "The ffmpeg is not compiled with NVENC support.\n\n"
+                "Please re-compile ffmpeg following the instructions at:\n    "
+                "https://docs.nvidia.com/video-technologies/video-codec-sdk/ffmpeg-with-nvidia-gpu/"
+            )
 
         if get_num_NVIDIA_GPUs() == 0:
-            raise RuntimeError('No NVIDIA GPU found.\n\n'
-                               'Please use a NVIDIA GPU card listed at:\n    '
-                               'https://developer.nvidia.com/video-encode-and-decode-gpu-support-matrix-new')
+            raise RuntimeError(
+                "No NVIDIA GPU found.\n\n"
+                "Please use a NVIDIA GPU card listed at:\n    "
+                "https://developer.nvidia.com/video-encode-and-decode-gpu-support-matrix-new"
+            )
 
         _check_nvidia_init = True
 
     return True
 
 
-def VideoCapture(file, 
-                 codec=None, 
-                 pix_fmt='bgr24',
-                 crop_xywh=None,
-                 resize=None,
-                 resize_keepratio=True,
-                 resize_keepratioalign='center'):
+def VideoCapture(
+    file,
+    codec=None,
+    pix_fmt="bgr24",
+    crop_xywh=None,
+    resize=None,
+    resize_keepratio=True,
+    resize_keepratioalign="center",
+):
     """
     Alternative to cv2.VideoCapture
 
@@ -125,25 +137,22 @@ def VideoCapture(file,
     ```
     cap = ffmpegcv.VideoCapture(file, resize=(640, 480), resize_keepratio=True)
     ```
-    
+
     Crop and then resize the video.
     ```python
     cap = ffmpegcv.VideoCapture(file, crop_xywh=(0, 0, 640, 480), resize=(512, 512))
     ```
     Author: Chenxinfeng 2022-04-16, cxf529125853@163.com
     """
-    return FFmpegReader.VideoReader(file, codec, pix_fmt, crop_xywh,
-                                    resize, resize_keepratio, resize_keepratioalign)
+    return FFmpegReader.VideoReader(
+        file, codec, pix_fmt, crop_xywh, resize, resize_keepratio, resize_keepratioalign
+    )
 
 
-VideoReader=VideoCapture                                                                 
+VideoReader = VideoCapture
 
 
-def VideoWriter(file, 
-                codec=None, 
-                fps=30, 
-                frameSize=None, 
-                pix_fmt='bgr24'):
+def VideoWriter(file, codec=None, fps=30, frameSize=None, pix_fmt="bgr24"):
     """
     Alternative to cv2.VideoWriter
 
@@ -156,7 +165,7 @@ def VideoWriter(file,
     fps : number
         Frames per second. Optional. Default is 30.
     frameSize : tuple
-        Frame size. (width, height). Optional. Default is `None`, which is 
+        Frame size. (width, height). Optional. Default is `None`, which is
         decided by the size of the first frame.
     pix_fmt : str
         Pixel format of input. ['bgr24' | 'rgb24']. Optional. Default is 'bgr24'.
@@ -166,8 +175,8 @@ def VideoWriter(file,
     opencv
     ```
     out = cv2.VideoWriter('outpy.avi',
-                          cv2.VideoWriter_fourcc('M','J','P','G'), 
-                          10, 
+                          cv2.VideoWriter_fourcc('M','J','P','G'),
+                          10,
                           (w, h))
     out.write(frame1)
     out.write(frame2)
@@ -204,29 +213,28 @@ def VideoWriter(file,
     return FFmpegWriter.VideoWriter(file, codec, fps, frameSize, pix_fmt)
 
 
-def VideoCaptureNV(file,
-                   pix_fmt='bgr24',
-                   crop_xywh=None,
-                   resize=None,
-                   resize_keepratio=True,
-                   resize_keepratioalign='center',
-                   gpu=0):
+def VideoCaptureNV(
+    file,
+    pix_fmt="bgr24",
+    crop_xywh=None,
+    resize=None,
+    resize_keepratio=True,
+    resize_keepratioalign="center",
+    gpu=0,
+):
     """
     `ffmpegcv.VideoCaptureNV` is a gpu version for `ffmpegcv.VideoCapture`.
     """
     _check_nvidia()
-    return FFmpegReaderNV.VideoReader(file, pix_fmt, crop_xywh, 
-                                      resize, resize_keepratio, resize_keepratioalign, 
-                                      gpu)
+    return FFmpegReaderNV.VideoReader(
+        file, pix_fmt, crop_xywh, resize, resize_keepratio, resize_keepratioalign, gpu
+    )
+
 
 VideoReaderNV = VideoCaptureNV
 
-def VideoWriterNV(file,
-                  codec=None,
-                  fps=30,
-                  frameSize=None,
-                  pix_fmt='bgr24',
-                  gpu=0):
+
+def VideoWriterNV(file, codec=None, fps=30, frameSize=None, pix_fmt="bgr24", gpu=0):
     """
     `ffmpegcv.VideoWriterNV` is a gpu version for `ffmpegcv.VideoWriter`.
     """
@@ -234,19 +242,30 @@ def VideoWriterNV(file,
     return FFmpegWriterNV.VideoWriter(file, codec, fps, frameSize, pix_fmt, gpu)
 
 
-def VideoCaptureCAM(camname,
-                    pix_fmt='bgr24',
-                    crop_xywh=None,
-                    resize=None,
-                    resize_keepratio=True,
-                    resize_keepratioalign='center',
-                    camsize_wh=None,
-                    camfps=None,
-                    camcodec=None,
-                    campix_fmt=None):
-    return FFmpegReaderCAM.VideoReader(camname, pix_fmt, crop_xywh,
-        resize, resize_keepratio, resize_keepratioalign,
-        camsize_wh=camsize_wh, camfps=camfps, camcodec=camcodec,
-        campix_fmt=campix_fmt)
+def VideoCaptureCAM(
+    camname,
+    pix_fmt="bgr24",
+    crop_xywh=None,
+    resize=None,
+    resize_keepratio=True,
+    resize_keepratioalign="center",
+    camsize_wh=None,
+    camfps=None,
+    camcodec=None,
+    campix_fmt=None,
+):
+    return FFmpegReaderCAM.VideoReader(
+        camname,
+        pix_fmt,
+        crop_xywh,
+        resize,
+        resize_keepratio,
+        resize_keepratioalign,
+        camsize_wh=camsize_wh,
+        camfps=camfps,
+        camcodec=camcodec,
+        campix_fmt=campix_fmt,
+    )
+
 
 VideoReaderCAM = VideoCaptureCAM
--- ffmpegcv/ffmpeg_writer.py
+++ ffmpegcv/ffmpeg_writer.py
@@ -5,7 +5,7 @@ import sys
 from .video_info import run_async, release_process, get_num_NVIDIA_GPUs
 
 
-IN_COLAB = 'google.colab' in sys.modules
+IN_COLAB = "google.colab" in sys.modules
 
 
 class FFmpegWriter:
@@ -14,7 +14,7 @@ class FFmpegWriter:
 
     def __enter__(self):
         return self
-    
+
     def __exit__(self, type, value, traceback):
         self.release()
 
@@ -22,20 +22,22 @@ class FFmpegWriter:
         self.release()
 
     def __repr__(self):
-        props = pprint.pformat(self.__dict__).replace('{',' ').replace('}',' ')
-        return f'{self.__class__}\n'  + props
+        props = pprint.pformat(self.__dict__).replace("{", " ").replace("}", " ")
+        return f"{self.__class__}\n" + props
 
     @staticmethod
     def VideoWriter(filename, codec, fps, frameSize, pix_fmt):
         if codec is None:
-            codec = 'h264'
+            codec = "h264"
         elif not isinstance(codec, str):
-            codec = 'h264'
-            warnings.simplefilter('''
+            codec = "h264"
+            warnings.simplefilter(
+                """
                 Codec should be a string. Eg `h264`, `h264_nvenc`. 
                 You may used CV2.VideoWriter_fourcc, which will be ignored.
-                ''')
-        
+                """
+            )
+
         vid = FFmpegWriter()
         vid.fps, vid.size = fps, frameSize
         vid.width, vid.height = vid.size if vid.size else (None, None)
@@ -44,26 +46,28 @@ class FFmpegWriter:
         return vid
 
     def _init_video_stream(self):
-        args = (f'ffmpeg -y -loglevel warning ' 
-                f'-f rawvideo -pix_fmt {self.pix_fmt} -s {self.width}x{self.height} -r {self.fps} -i pipe: '
-                f'-r {self.fps} -c:v {self.codec} -pix_fmt yuv420p "{self.filename}"')
+        args = (
+            f"ffmpeg -y -loglevel warning "
+            f"-f rawvideo -pix_fmt {self.pix_fmt} -s {self.width}x{self.height} -r {self.fps} -i pipe: "
+            f'-r {self.fps} -c:v {self.codec} -pix_fmt yuv420p "{self.filename}"'
+        )
         self.process = run_async(args)
 
     def write(self, img):
         if self.waitInit:
             if self.size is None:
-                self.size = (img.shape[1], img.shape[0])          
+                self.size = (img.shape[1], img.shape[0])
             self.width, self.height = self.size
             self._init_video_stream()
             self.waitInit = False
-        
+
         self.iframe += 1
         assert self.size == (img.shape[1], img.shape[0])
         img = img.astype(np.uint8).tobytes()
         self.process.stdin.write(img)
 
     def release(self):
-        if hasattr(self, 'process'):
+        if hasattr(self, "process"):
             release_process(self.process)
 
     def close(self):
@@ -77,18 +81,23 @@ class FFmpegWriterNV(FFmpegWriter):
         assert numGPU
         gpu = int(gpu) % numGPU if gpu is not None else 0
         if codec is None:
-            codec = 'hevc_nvenc'
+            codec = "hevc_nvenc"
         elif not isinstance(codec, str):
-            codec = 'hevc_nvenc'
-            warnings.simplefilter('''
+            codec = "hevc_nvenc"
+            warnings.simplefilter(
+                """
                 Codec should be a string. Eg `h264`, `h264_nvenc`. 
                 You may used CV2.VideoWriter_fourcc, which will be ignored.
-                ''')
-        elif codec.endswith('_nvenc'):
+                """
+            )
+        elif codec.endswith("_nvenc"):
             codec = codec
         else:
-            codec = codec + '_nvenc'
-        assert codec in ['hevc_nvenc', 'h264_nvenc'], 'codec should be `hevc_nvenc` or `h264_nvenc`'
+            codec = codec + "_nvenc"
+        assert codec in [
+            "hevc_nvenc",
+            "h264_nvenc",
+        ], "codec should be `hevc_nvenc` or `h264_nvenc`"
 
         vid = FFmpegWriterNV()
         vid.fps, vid.size = fps, frameSize
@@ -99,10 +108,12 @@ class FFmpegWriterNV(FFmpegWriter):
         return vid
 
     def _init_video_stream(self):
-        default_preset = 'default' if IN_COLAB else 'p2'
-        self.preset = getattr(self, 'preset', default_preset)
-        args = (f'ffmpeg -y -loglevel warning '
-            f'-f rawvideo -pix_fmt {self.pix_fmt} -s {self.width}x{self.height} -r {self.fps} -i pipe: '
-            f'-preset {self.preset} '
-            f'-r {self.fps} -gpu {self.gpu} -c:v {self.codec} -pix_fmt yuv420p "{self.filename}"')
+        default_preset = "default" if IN_COLAB else "p2"
+        self.preset = getattr(self, "preset", default_preset)
+        args = (
+            f"ffmpeg -y -loglevel warning "
+            f"-f rawvideo -pix_fmt {self.pix_fmt} -s {self.width}x{self.height} -r {self.fps} -i pipe: "
+            f"-preset {self.preset} "
+            f'-r {self.fps} -gpu {self.gpu} -c:v {self.codec} -pix_fmt yuv420p "{self.filename}"'
+        )
         self.process = run_async(args)
