cfef5048fa116872d54a5d80c2677d4962d36237
chenxinfeng <chenxinfeng@cibr.ac.cn>
2024-04-30 17:50:58 +0800

    add gray pixel format in writer

--- ffmpegcv/ffmpeg_writer_stream_realtime.py
+++ ffmpegcv/ffmpeg_writer_stream_realtime.py
@@ -6,7 +6,7 @@ class FFmpegWriterStreamRT(FFmpegWriter):
     @staticmethod
     def VideoWriter(filename:str, codec, pix_fmt, bitrate=None, resize=None) -> FFmpegWriter:
         assert codec in ['h264', 'libx264', 'x264']
-        assert pix_fmt in ['bgr24', 'rgb24']
+        assert pix_fmt in ['bgr24', 'rgb24', 'gray']
         # assert filename.startswith('rtmp://'), 'currently only support rtmp'
         assert resize is None or len(resize) == 2
         vid = FFmpegWriterStreamRT()
